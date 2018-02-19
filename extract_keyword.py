2ab62fba03a0cc70e451dbd0518c4b93f90c27a4
Lily Xu <lilyxu@alum.mit.edu>
2018-02-18 21:45:40 -0500

    Update filter for advertisements

--- extract_keyword.py
+++ extract_keyword.py
@@ -2,7 +2,6 @@ from sparkcc import CCSparkJob
 from pyspark.sql.types import StructType, StructField, StringType, LongType
 import ujson as json
 
-
 class ExtractKeywordJob(CCSparkJob):
     """ Extract keywords from title in Common Crawl WAT files """
 
@@ -10,7 +9,10 @@ class ExtractKeywordJob(CCSparkJob):
 
     output_schema = StructType([
         StructField("url", StringType(), True),
-        StructField("keywords", StringType(), True)])
+        StructField("keywords", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("description", StringType(), True),
+        ])
 
     def process_record(self, record):
 		''' returns list of keywords given WAT file'''
@@ -18,11 +20,11 @@ class ExtractKeywordJob(CCSparkJob):
 			record = json.loads(record.content_stream().read())
 			url = self.get_url(record)
 			title = self.get_title(record)
-			ascii_title = self.is_title_ascii(title)  # remove non-english
 			links = self.get_links(record)
 
-			if title and ascii_title:
-				 # and not has_ads(links)
+			if title and self.is_english(title) and not self.has_ads(links):
+				descrip = self.get_description(record)
+
 				title_words = [
 					word for word in title.lower().split() \
 					if not word.isdigit() and 
@@ -30,7 +32,7 @@ class ExtractKeywordJob(CCSparkJob):
 					word.isalnum()
 				]
 				for word in title_words: 
-					yield url, word		
+					yield url, word, title, descrip
 
     def run_job(self, sc, sqlc):
         # convert .gz input file to RDD of strings
@@ -50,57 +52,101 @@ class ExtractKeywordJob(CCSparkJob):
         self.get_logger(sc).info('records processed = {}'.format(
             self.records_processed.value))
 
-	# helper functions
     def is_wat_json_record(self, record):
 		''' Return true if WARC record is a WAT record'''
 		return (record.rec_type == 'metadata' and
 	                record.content_type == 'application/json')
 
     def get_url(self, record):
+    	# not using safe access .get on dict since so many key-value pairs could be missing
 		try:
 			return record['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
 		except KeyError, e:       # missing in metadata
-			return None
+			return ''
 
     def get_title(self, record):
 		try:
 			return record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Head']['Title']
 		except KeyError, e:       # missing in metadata
-			return None
+			return ''
 
-    def is_title_ascii(self, title):
+    def get_description(self, record):
+		try:
+			metas = record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Head']['Metas']
+			for mdict in metas:
+				if mdict.get('name','').lower() == 'description':
+					return mdict['content']	
+			return ''
+
+		except KeyError, e:       # missing in metadata
+			return ''
+
+    def get_headline(self, record):
+		try:
+			metas = record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Head']['Metas']
+			for mdict in metas:
+				if mdict.get('name','').lower() == 'headline':
+					return mdict['content']	
+			return ''
+
+		except KeyError, e:       # missing in metadata
+			return ''
+
+    def get_page_keywords(self, record):
+		try:
+			metas = record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Head']['Metas']
+			for mdict in metas:
+				if mdict.get('name','').lower() == 'keywords':
+					return mdict['content']	
+			return ''
+
+		except KeyError, e:       # missing in metadata
+			return ''
+
+    def get_section(self, record):
+		try:
+			metas = record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Head']['Metas']
+			for mdict in metas:
+				if mdict.get('name','').lower() == 'section':
+					return mdict['content']	
+			return ''
+
+		except KeyError, e:       # missing in metadata
+			return ''
+
+    def is_english(self, title):
+	    # if title cannot be ascii-encoded, then assume english
 		try: 
 			return title.decode('ascii')
-		except:  ## TODO list specific type of Error
-			return None
+		except UnicodeEncodeError:  
+			return False
 
     def get_links(self, record):
 		try:
 			return record['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
 		except KeyError, e:       # missing in metadata
-			return None
-
-
-	### YOU ARE HERE ###
-	### this function needs work ###
-    def has_ads(self, links):
-		''' Takes links (list of dictionaries) and returns boolean for whether or not links contain 'adsense' '''
-		adwords_path = 'adwords.txt'
-		with open(adwords_path, 'r') as myfile:     
-			adwords = myfile.readlines()
-
-		for d in links:  
-			if d.get('url', None):   # .get can return Nonetype,
-									 # which has no attribute "encode"
-				url = d.get('url').encode('ascii','ignore').lower()   
-				if any(x in url for x in adwords):     
-					return True
+			return []
 
     def is_stopword(self, word):
 		stopwords_path = 'stopwords.txt'
 		with open(stopwords_path, 'r') as myfile:     
 			stopwords = myfile.readlines()[0].split(',')
 		return word in stopwords
+
+    def has_ads(self, links):
+		''' Takes links (list of dictionaries)
+		Returns boolean for whether or not links contain adwords'''
+		adwords = []
+		with open('adwords.txt') as myfile:     
+			for line in myfile:
+				adwords.append(line.strip())
+
+		for d in links:  
+			if d.get('url', None):   # .get can return Nonetype,
+									 # which has no attribute "encode"
+				url = d.get('url', u'').encode('ascii','ignore').lower()
+				if any(x in url for x in adwords):  
+					return True
 
 
 if __name__ == '__main__':
