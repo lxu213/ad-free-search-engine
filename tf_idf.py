49a41ea7700b96cf0118aac96fcdc7778d4a3c10
Lily Xu <lilyxu@alum.mit.edu>
2018-03-05 13:01:26 -0800

    Rank by term frequency - inverse document frequency.

--- tf_idf.py
+++ tf_idf.py
@@ -4,27 +4,45 @@ import numpy as np
 import pandas as pd
 import pyarrow.parquet as pq
 from fastparquet import write
+import time
 
-PQPATH='/Users/lxu213/data/ad-free-search-engine/spark-warehouse/add_count/part-00000-6d70eeb9-5f8f-450b-ad70-f431c336e72d-c000.snappy.parquet'
+# runtime O(n) on 1 parquet = 30 min
+# TODO: how to run this at scale (parallelizing or sampling)
+
+PQPATH='/Users/lxu213/data/ad-free-search-engine/spark-warehouse/has_rank/part-00000-5ede3ccf-f2ae-4379-865c-a660d10c03b4-c000.snappy.parquet'
 OUTPATH='/Users/lxu213/data/ad-free-search-engine/spark-warehouse/'
 
-warc = pq.read_table(PQPATH, nthreads=4).to_pandas()  
+warc = pq.read_table(PQPATH, nthreads=4).to_pandas()
 
-def calculate_tf_idf(warc):
+def build_idf_dict(warc):
+
+    tot_doc = len(warc)/10       # total documents
+    idf_dict = {}
+
+    for term in set(warc['keywords']):
+        idf_dict[term] = np.log(tot_doc/len(warc.loc[warc['keywords'] == term]))
+
+    return idf_dict
+
+def calculate_tf_idf(warc, idf_dict):
 
     warc['tf-idf'] = pd.Series(0, index=warc.index)
-    tot_doc = len(warc)/10       # total documents
 
-    for i in range(len(warc)):
-        docs_w_term = len(warc.loc[warc['keywords'] == warc['keywords'][i]])
-        warc.loc[i,'tf-idf'] = warc['val'][i]['count'] * np.log(tot_doc/docs_w_term)
+    for idx, keyword in enumerate(warc['keywords']):
+        warc.loc[idx,'tf-idf'] = warc['val'][idx]['count'] * idf_dict.get(keyword, 1.0)
         
+    return warc
+ 
 def save_parquet(result):
 
-    write(OUTPATH + 'add_count/tf_idf.parquet', warc, compression='snappy')
+    write(OUTPATH + 'has_rank/testing_optimization.parquet', warc, compression='snappy')
 
 
 
 if __name__ == '__main__':
-    result = calculate_tf_idf(warc)
+
+    idf_dict = build_idf_dict(warc)
+    result = calculate_tf_idf(warc, idf_dict)
     save_parquet(result)
+
+
